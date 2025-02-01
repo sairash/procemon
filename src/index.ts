0242eadfed23239b86a32c1e2d33d7d75bf6a68b
Sai On Action <gautamsairash@gmail.com>
2025-02-02 04:07:56 +0545

    no cache for github test

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,26 @@
 import Procemon from "./procemon";
-export interface Env {}
+export interface Env { }
 
 export default {
-  async fetch(request: Request): Promise<Response> {
-    const url = new URL(request.url);
-    const params = url.searchParams;
+	async fetch(request: Request): Promise<Response> {
+		const url = new URL(request.url);
+		const params = url.searchParams;
 
-    const size = params.get('size') || '32';
-    const color = params.get('color') || 'white';
-    const background = params.get('background') || 'black';
+		const size = params.get('size') || '32';
+		const color = params.get('color') || 'white';
+		const background = params.get('background') || 'black';
 
-    const p = new Procemon(parseInt(size));
-    const svg = p.plot_svg(background, color);
+		const p = new Procemon(parseInt(size));
+		const svg = p.plot_svg(background, color);
 
 
-    return new Response(svg, {
-      headers: { 'Content-Type': 'image/svg+xml' }
-    });
-  }
+		return new Response(svg, {
+			headers: {
+				'Content-Type': 'image/svg+xml',
+				'Cache-Control': 'no-cache, no-store, must-revalidate',
+				'Pragma': 'no-cache',
+				'Expires': '0'
+			}
+		});
+	}
 };
